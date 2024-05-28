521a370c0a4af918e8fc6d9ed4e67efbf3615395
etachott <bfwaend@gmail.com>
2024-05-28 19:49:40 -0300

    Add basic flag setup

--- src/main.c
+++ src/main.c
@@ -1,43 +1,30 @@
 #include "ft_ping.h"
 
 int main(int argc, char *argv[]) {
-    int opt;
-    int flag_f = 0, flag_v = 0, flag_e = 0;
+    int opt = 0;
+    bool flag_v = false;
 
-    // Parsing command line arguments using getopt
-    while ((opt = getopt(argc, argv, "fve")) != -1) {
-        switch (opt) {
-            case 'f':
-                flag_f = 1;
-                break;
-            case 'v':
-                flag_v = 1;
-                break;
-            case 'e':
-                flag_e = 1;
-                break;
-            default: /* '?' */
-                fprintf(stderr, "Usage: %s [-f] [-v] [-e]\n", argv[0]);
-                exit(EXIT_FAILURE);
-        }
+    if (argc == 1) {
+        print_help();
     }
 
-    // Handle -f flag
-    if (flag_f) {
-        printf("Flag -f is set.\n");
-        // Add your -f flag handling code here
+    // Parsing command line arguments using getopt
+    while ((opt = getopt(argc, argv, "v?")) != -1) {
+        switch (opt) {
+            case 'v':
+                flag_v = true;
+                break;
+            case '?':
+                print_help();
+            default:
+                print_help();
+        }
     }
 
     // Handle -v flag
     if (flag_v) {
         printf("Flag -v is set.\n");
         // Add your -v flag handling code here
-    }
-
-    // Handle -e flag
-    if (flag_e) {
-        printf("Flag -e is set.\n");
-        // Add your -e flag handling code here
     }
 
     return 0;
