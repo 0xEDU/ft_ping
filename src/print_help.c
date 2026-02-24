9d3a1433d9ad12bf720b32dd95d71dfc430fcdb2
Eduardo Tachotte <bfwaend@gmail.com>
2026-02-23 21:53:12 -0300

    Format print_help function

--- src/print_help.c
+++ src/print_help.c
@@ -1,12 +1,12 @@
 #include "ft_ping.h"
 
 void print_help() {
-    printf("Usage: ft_ping [OPTION...] HOST ...\n");
-    printf("Send ICMP ECHO_REQUEST packets to network hosts.\n\n");
-    printf(" Options valid for all request types:\n\n");
-    printf("  -v       verbose output\n\n");
-    printf(" Print this message:\n\n");
-    printf("  -?       give this help list\n\n");
-    printf("Made with love by Edu :)\n");
-    exit(EXIT_SUCCESS);
+  printf("Usage: ft_ping [OPTION...] HOST ...\n");
+  printf("Send ICMP ECHO_REQUEST packets to network hosts.\n\n");
+  printf(" Options valid for all request types:\n\n");
+  printf("  -v       verbose output\n\n");
+  printf(" Print this message:\n\n");
+  printf("  -?       give this help list\n\n");
+  printf("Made with love by Edu :)\n");
+  exit(EXIT_SUCCESS);
 }
